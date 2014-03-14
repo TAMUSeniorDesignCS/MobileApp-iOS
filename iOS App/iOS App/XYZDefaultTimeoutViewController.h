7be2629f2da6e6b588d03371151ca7ae94be5b04
sujinlee90 <slee@imac-d25hm0q3dhjw.cs.tamu.edu>
2014-03-14 17:24:23 -0500

    Default Timeout has two groups(Post and Message)

---
+++ iOS App/iOS App/XYZDefaultTimeoutViewController.h
@@ -0,0 +1,17 @@
+//
+//  XYZDefaultTimeoutViewController.h
+//  iOS App
+//
+//  Created by Sujin Lee on 3/14/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZDefaultTimeoutViewController : UITableViewController
+@property (weak, nonatomic) IBOutlet UISwitch *PostSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *MessageSwitch;
+@property (weak, nonatomic) IBOutlet UITableViewCell *PostHours;
+@property (weak, nonatomic) IBOutlet UITableViewCell *MessageHours;
+
+@end
