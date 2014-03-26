dfa363b277d01d37937240d7d01b52c9b466bd88
John Nowotny <john.nowotny@gmail.com>
2014-03-26 16:40:49 -0500

    login works!!!

--- iOS App/iOS App/XYZLoginView.m
+++ iOS App/iOS App/XYZLoginView.m
@@ -35,10 +35,48 @@
 
 - (IBAction)pressLoginButton:(id)sender{
     
-    if([self.Username.text isEqual:@"jnowotny"]) {
-        [self performSegueWithIdentifier:@"tabBarPush" sender:nil];
-    }else if([self.Username.text isEqual:@"sujin"]){
-        [self performSegueWithIdentifier:@"tabBarPush" sender:nil];
+    NSMutableURLRequest *request = [NSMutableURLRequest
+                                    requestWithURL:[NSURL URLWithString:@"http://ec2-54-201-163-32.us-west-2.compute.amazonaws.com:80/member/auth"]];
+    
+    NSDictionary *requestData = [[NSDictionary alloc] initWithObjectsAndKeys:
+                                 self.Username.text, @"username",
+                                 self.Password.text, @"password",
+                                 nil];
+    NSError *error;
+    NSData *postData = [NSJSONSerialization dataWithJSONObject:requestData options:0 error:&error];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [request setHTTPMethod:@"POST"];
+    [request setHTTPBody:postData];
+    NSData *authData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:&error];
+    NSString *authReturn = [[NSString alloc] initWithData:authData encoding:NSUTF8StringEncoding];
+    NSLog(@"post string is %@", authReturn);
+
+    /*
+    NSString *url = [NSString stringWithFormat:@"http://ec2-54-201-163-32.us-west-2.compute.amazonaws.com:80/member/auth?username=%@&password=%@",self.Username.text,self.Password.text];
+    
+	NSLog(@"usr is %@", url);
+    
+    // Prepare URL request to download statuses from Twitter
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
+    // Error
+    NSError *error;
+    // Perform request and get JSON back as a NSData object
+    NSData *authData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:&error];
+    NSString *authReturn = [[NSString alloc] initWithData:authData encoding:NSUTF8StringEncoding];
+    NSLog(@"post string is %@", authReturn);
+    */
+    
+    if (error) {
+        NSLog(@"error: %@", [error localizedDescription]);
+    }
+    else{
+        if (![authReturn isEqualToString:@"NO"]) {
+            [self performSegueWithIdentifier:@"tabBarPush" sender:nil];
+        }
+        else {
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Cannot login" message:@"Username and/or password not recognized." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [alertView show];
+        }
     }
 }
 
