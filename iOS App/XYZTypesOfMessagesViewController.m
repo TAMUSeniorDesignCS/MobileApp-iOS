afc2e8e1378195abd721566fbe10837fafad3fce
John Nowotny <john.nowotny@gmail.com>
2014-04-12 15:57:05 -0500

    settings for type of messages implemented

--- iOS App/XYZTypesOfMessagesViewController.m
+++ iOS App/XYZTypesOfMessagesViewController.m
@@ -7,12 +7,15 @@
 //
 
 #import "XYZTypesOfMessagesViewController.h"
+#import "XYZAppDelegate.h"
 
 @interface XYZTypesOfMessagesViewController ()
 
 @end
 
 @implementation XYZTypesOfMessagesViewController
+
+@synthesize inspirationalCell, funnyCell, religiousCell;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -26,20 +29,9 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
 	// Do any additional setup after loading the view.
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    BOOL religiousState = [defaults boolForKey:@"rel"];
-    if (religiousState) self.religiousCell.accessoryType = UITableViewCellAccessoryCheckmark;
-    else self.religiousCell.accessoryType = UITableViewCellAccessoryNone;
     
-    
-    BOOL funnyState = [defaults boolForKey:@"fun"];
-    if (funnyState) self.funnyCell.accessoryType = UITableViewCellAccessoryCheckmark;
-    else self.funnyCell.accessoryType = UITableViewCellAccessoryNone;
-    
-    BOOL inspirationalState = [defaults boolForKey:@"ins"];
-    if (inspirationalState) self.inspirationalCell.accessoryType = UITableViewCellAccessoryCheckmark;
-    else self.inspirationalCell.accessoryType = UITableViewCellAccessoryNone;
 
 }
 
@@ -49,6 +41,30 @@
     // Release any retained subviews of the main view.
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    //INDEX KEY 0:RELIGIOUS
+    //1:FUNNY
+    //2:INSPIRATIONAL
+    XYZAppDelegate *appDelegate=(XYZAppDelegate *)[UIApplication sharedApplication].delegate;
+    //UITableViewCell *religious = [self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
+    //UITableViewCell *funny = [self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:1 inSection:0]];
+    //UITableViewCell *inspirational = [self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:2 inSection:0]];
+    
+    if (appDelegate.userSettings.inspirationalOn)
+        self.inspirationalCell.accessoryType = UITableViewCellAccessoryCheckmark;
+    else
+        inspirationalCell.accessoryType = UITableViewCellAccessoryNone;
+    
+    if (appDelegate.userSettings.funnyOn)
+        funnyCell.accessoryType = UITableViewCellAccessoryCheckmark;
+    else
+        funnyCell.accessoryType = UITableViewCellAccessoryNone;
+    
+    if (appDelegate.userSettings.religiousOn)
+        religiousCell.accessoryType = UITableViewCellAccessoryCheckmark;
+    else
+        religiousCell.accessoryType = UITableViewCellAccessoryNone;
+}
 
 - (void)didReceiveMemoryWarning
 {
@@ -58,28 +74,25 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    XYZAppDelegate *appDelegate=(XYZAppDelegate *)[UIApplication sharedApplication].delegate;
+
     //INDEX KEY 0:RELIGIOUS
     //1:FUNNY
     //2:INSPIRATIONAL
     UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
     if (cell.accessoryType == UITableViewCellAccessoryCheckmark) {
         cell.accessoryType = UITableViewCellAccessoryNone;
-        if (indexPath.row == 0) [defaults setBool:FALSE forKey:@"rel"];
-        else if (indexPath.row == 1) [defaults setBool:FALSE forKey:@"fun"];
-        else if (indexPath.row == 2) [defaults setBool:FALSE forKey:@"ins"];
-        [defaults synchronize];
+        if (indexPath.row == 0) appDelegate.userSettings.religiousOn = FALSE;
+        else if (indexPath.row == 1) appDelegate.userSettings.funnyOn = FALSE;
+        else if (indexPath.row == 2) appDelegate.userSettings.inspirationalOn = FALSE;
     }
     else {
         cell.accessoryType = UITableViewCellAccessoryCheckmark;
-        if (indexPath.row == 0) [defaults setBool:TRUE forKey:@"rel"];
-        else if (indexPath.row == 1) [defaults setBool:TRUE forKey:@"fun"];
-        else if (indexPath.row == 2) [defaults setBool:TRUE forKey:@"ins"];
-        [defaults synchronize];
+        if (indexPath.row == 0) appDelegate.userSettings.religiousOn = YES;
+        else if (indexPath.row == 1) appDelegate.userSettings.funnyOn = TRUE;
+        else if (indexPath.row == 2) appDelegate.userSettings.inspirationalOn = TRUE;
     }
 }
-
-
 
 
 @end
